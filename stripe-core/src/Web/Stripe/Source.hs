92786f29b57795055d13b8bb96b41827c3921574
Gavin Whelan <gavwhela@gmail.com>
2018-01-02 00:47:35 +0000

    Add customerAttachSource call.

--- stripe-core/src/Web/Stripe/Source.hs
+++ stripe-core/src/Web/Stripe/Source.hs
@@ -4,11 +4,42 @@
 {-# LANGUAGE TypeFamilies          #-}
 module Web.Stripe.Source
     ( -- * API
+      customerAttachSource
+    , CustomerAttachSource
       -- * Types
-      Source (..)
+    , Source (..)
     , SourceId (..)
+    , SourceValue (..)
+    , SourceUsage (..)
+    , SourceStatus (..)
+    , SourceOwner (..)
+    , SourceAuthFlow (..)
     ) where
 
-import Web.Stripe.Types ( Source (..)
-                        , SourceId (..))
-import Web.Stripe.Types.Util
+import           Web.Stripe.StripeRequest (Method (GET, POST, DELETE),
+                                           StripeHasParam, StripeRequest (..),
+                                           StripeReturn, ToStripeParam(..),
+                                           mkStripeRequest)
+import           Web.Stripe.Util          ((</>))
+import           Web.Stripe.Types         ( Source (..)
+                                          , SourceId (..)
+                                          , SourceValue (..)
+                                          , SourceUsage (..)
+                                          , SourceStatus (..)
+                                          , SourceOwner (..)
+                                          , SourceAuthFlow (..)
+                                          , CustomerId (..))
+import           Web.Stripe.Types.Util    (getCustomerId)
+
+customerAttachSource :: CustomerId
+                     -> SourceId
+                     -> StripeRequest CustomerAttachSource
+customerAttachSource
+  customerId
+  sourceId      = request
+  where request = mkStripeRequest POST url params
+        url     = "customers" </> (getCustomerId customerId) </> "sources"
+        params  = toStripeParam sourceId []
+
+data CustomerAttachSource
+type instance StripeReturn CustomerAttachSource = Source
